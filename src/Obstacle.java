bcde1bb3ab542723f714a24851528cf9b60da7bd
Raffaele Piccini <lele.raffa97@gmail.com>
2016-03-16 13:12:42 +0100

    point class added

--- src/Obstacle.java
+++ src/Obstacle.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 
 public interface Obstacle {
 	
@@ -5,15 +6,6 @@ public interface Obstacle {
 	public double getBouncyness() ;
 	public void setLocation(double x, double y, double z) ;
 	public double[] getLocation() ;
-	public void setCorner2(double x, double y, double z) ;
-	public void setCorner3(double x, double y, double z) ;
-	public void setCorner4(double x, double y, double z) ; 
-	public double[] getCorner2() ;
-	public double[] getCorner3() ;
-	public double[] getCorner4() ;
-	public double[][] getSide1(double[] corner1, double[] corner2) ;
-	public double[][] getside2(double[] corner2, double[] corner3) ;
-	public double[][] getSide3(double[] corner3, double[] corner4) ;
-	public double[][] getSide4(double[] corner4, double[] corner1) ;
+	public ArrayList<double[]> getVertices();
 
 }
