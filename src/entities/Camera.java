2fb68c6919cf6db7ff0a0f3354b691e40e2d8eb8
Raffaele Piccini <lele.raffa97@gmail.com>
2016-04-08 11:08:48 +0200

    ccc

--- src/entities/Camera.java
+++ src/entities/Camera.java
@@ -14,22 +14,22 @@ public class Camera {
 	
 	public void move(){
 		if(Keyboard.isKeyDown(Keyboard.KEY_W)){
-			position.z-=0.2f;
+			position.z-=0.5f;
 		}
 		if(Keyboard.isKeyDown(Keyboard.KEY_S)){
-			position.z+=0.2f;
+			position.z+=0.5f;
 		}
 		if(Keyboard.isKeyDown(Keyboard.KEY_D)){
-			position.x+=0.2f;
+			position.x+=0.5f;
 		}
 		if(Keyboard.isKeyDown(Keyboard.KEY_A)){
-			position.x-=0.2f;
+			position.x-=0.5f;
 		}
 		if(Keyboard.isKeyDown(Keyboard.KEY_SPACE)){
-			position.y+=0.2f;
+			position.y+=0.5f;
 		}
 		if(Keyboard.isKeyDown(Keyboard.KEY_LSHIFT)){
-			position.y-=0.2f;
+			position.y-=0.5f;
 		}
 	}
 
