2482c3b82218f687b9c7d076024bb18da6447c5d
Raffaele Piccini <lele.raffa97@gmail.com>
2016-05-13 13:54:41 +0200

    line  class added

---
+++ src/toolbox/Line.java
@@ -0,0 +1,18 @@
+package toolbox;
+
+import org.lwjgl.util.vector.Vector2f;
+
+public class Line {
+	
+	private float p;
+	private float m;
+	
+	public Line(Vector2f vector, Vector2f point){
+		m=vector.y/vector.x;
+		p=point.y-(m*point.x);
+	}
+	
+	public boolean liesOnSameSide(Vector2f p1, Vector2f p2){
+		
+	}
+}
