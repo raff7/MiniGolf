cdfb7f715665cdbe05dd001f61eef89accbaf19a
Raffaele Piccini <lele.raffa97@gmail.com>
2016-06-17 10:58:50 +0200

    m

--- src/engineTester/SampleCourse.java
+++ src/engineTester/SampleCourse.java
@@ -1,9 +1,15 @@
 package engineTester;
 
+import java.util.ArrayList;
+
 import org.lwjgl.util.vector.Vector3f;
+
+import bot.Node;
+import bot.NodeNetwork;
 import entities.Course;
 import entities.Entity;
 import entities.Light;
+import geometry.Triangle;
 import models.RawModel;
 import models.TexturedModel;
 import renderEngine.Loader;
@@ -17,33 +23,18 @@ public class SampleCourse {
 		
 		Course course = new Course();
 		
-		course.setStartingPosition(new Vector3f(40,100,40));
+		course.setStartingPosition(new Vector3f(10,100,40));
 		
-		RawModel rw = OBJLoader.loadObjModel("finishPoint", loader);
+		RawModel rw = OBJLoader.loadObjModel("testNode", loader);
 		ModelTexture rwModel = new ModelTexture(loader.loadTexture("white"));
 		rwModel.setUseFakeLight(false);
 		rwModel.setHasTransparency(true);
 
-		Entity ent = new Entity (new TexturedModel(rw,rwModel),new Vector3f(0,0,0),0,0,0,15);
-		
-//Testing cubes
-RawModel rw2 = OBJLoader.loadObjModel("middlepiece", loader);
-ModelTexture rwModel2 = new ModelTexture(loader.loadTexture("white"));
-Entity ent2 = new Entity(new TexturedModel(rw2,rwModel2), new Vector3f(0,0,-89.2f),0,0,0,15);
-RawModel rw3 = OBJLoader.loadObjModel("cube", loader);
-ModelTexture rwModel3 = new ModelTexture(loader.loadTexture("white"));
-Entity ent3 = new Entity(new TexturedModel(rw3,rwModel3), new Vector3f(0,0,-250),0,0,0,100);
-RawModel rw4 = OBJLoader.loadObjModel("cube", loader);
-ModelTexture rwModel4 = new ModelTexture(loader.loadTexture("white"));
-Entity ent4 = new Entity(new TexturedModel(rw4,rwModel4), new Vector3f(0,13,-50),0,0,0,10);
-
-course.addEntity(ent4);
-course.addEntity(ent3);
-course.addEntity(ent2);
-
+		Entity ent = new Entity (new TexturedModel(rw,rwModel),new Vector3f(0,0,0),0,0,0,4);
+	
 
 	course.addEntity(ent);
-		
+
 
 
 		course.addTerrain(new Terrain(0,0,loader,new ModelTexture(loader.loadTexture("grass")),"heightMap"));
@@ -128,6 +119,8 @@ course.addEntity(ent2);
 		course.addLight(lamp2.getLight());*/
 		Light sun = new Light(new Vector3f(0,100000,100000),new Vector3f(1f,1f,1f));
 		course.addLight(sun);
+		
+		course.createNetwork();
 		
 		
 		return course;
--- src/geometry/Tester.java
+++ src/geometry/Tester.java
@@ -6,6 +6,7 @@ import org.lwjgl.util.vector.Vector3f;
 
 import bot.Node;
 import bot.NodeNetwork;
+import engineTester.SampleCourse;
 import entities.Hole;
 import models.RawModel;
 import renderEngine.Loader;
@@ -13,76 +14,82 @@ import renderEngine.OBJLoader;
 
 public class Tester {
 	public static void main(String[] arg){
-	
-		Vector3f p1 = new Vector3f(2,0,2);
-		Vector3f p2 = new Vector3f(3,0,2);
-		Vector3f p3 = new Vector3f(3,0,3);
-		Vector3f p4= new Vector3f(2,0,3);
-		Vector3f p5 = new Vector3f(2.5f,0,1);
-		Vector3f p6 = new Vector3f(4,0,2.5f);
-		Vector3f p7 = new Vector3f(2.5f,0,4);
-		Vector3f p8 = new Vector3f(1,0,2.5f);
-		Vector3f p9 = new Vector3f(4,0,2);
-		Vector3f p10 = new Vector3f(4,0,3);
-		Vector3f p11 = new Vector3f(5,0,3);
-		Vector3f p12 = new Vector3f(5,0,2);
-		Vector3f p13 = new Vector3f(5,0,2);
-		Vector3f p14 = new Vector3f(5,0,3);
+//		
+//	
+//		Vector3f p1 = new Vector3f(2,0,2);
+//		Vector3f p2 = new Vector3f(3,0,2);
+//		Vector3f p3 = new Vector3f(3,0,3);
+//		Vector3f p4= new Vector3f(2,0,3);
+//		Vector3f p5 = new Vector3f(2.5f,0,1);
+//		Vector3f p6 = new Vector3f(4,0,2.5f);
+//		Vector3f p7 = new Vector3f(2.5f,0,4);
+//		Vector3f p8 = new Vector3f(1,0,2.5f);
+//		Vector3f p9 = new Vector3f(4,0,2);
+//		Vector3f p10 = new Vector3f(4,0,3);
+//		Vector3f p11 = new Vector3f(5,0,3);
+//		Vector3f p12 = new Vector3f(5,0,2);
+//		Vector3f p13 = new Vector3f(5,0,2);
+//		Vector3f p14 = new Vector3f(5,0,3);
+//		
+//		Triangle triangle1 = new Triangle(p1,p2,p5);
+//		Triangle triangle2 = new Triangle(p1,p8,p4);
+//		Triangle triangle3 = new Triangle(p4,p7,p3);
+//		Triangle triangle4 = new Triangle(p3,p6,p2);
+//		Triangle triangle5 = new Triangle(p10,p11,p9);
+//		Triangle triangle6 = new Triangle(p11,p12,p9);
+//		Triangle triangle7 = new Triangle(p11,p14,p12);
+//		Triangle triangle8 = new Triangle(p14,p13,p12);
+//		
+//		ArrayList<Vector3f> points = new ArrayList<Vector3f>();
+//		points.add(p1);
+//		points.add(p2);
+//		points.add(p3);
+//		points.add(p4);
+//		
+//		//nodes
+//		Hole hole = new Hole(points);
+//		ArrayList<Node> nodesList = new ArrayList<Node>();
+//		Node node1 = new Node(triangle1,"1");
+//		Node node2 = new Node(triangle2,"2");
+//		Node node3 = new Node(triangle3,"3");
+//		Node node4 = new Node(triangle4,"4");
+//		Node node5 = new Node(triangle5,"5");
+//		Node node6 = new Node(triangle6,"6");
+//		Node node7 = new Node(triangle7,"7");
+//		Node node8 = new Node(triangle8,"8");
+//		
+//		nodesList.add(node1);
+//		nodesList.add(node2);
+//		nodesList.add(node3);
+//		nodesList.add(node4);
+//		nodesList.add(node5);
+//		nodesList.add(node6);
+//		nodesList.add(node7);
+//		nodesList.add(node8);
+//		
+//		for(int i=0;i<1000000;i++){
+//			nodesList.add(new Node(new Triangle(new Vector3f(getNumber(),getNumber(),getNumber()),new Vector3f(getNumber(),getNumber(),getNumber()),new Vector3f(getNumber(),getNumber(),getNumber())),"a"));
+//		}
+		SampleCourse.getCourse(new Loader());
 		
-		Triangle triangle1 = new Triangle(p1,p2,p5);
-		Triangle triangle2 = new Triangle(p1,p8,p4);
-		Triangle triangle3 = new Triangle(p4,p7,p3);
-		Triangle triangle4 = new Triangle(p3,p6,p2);
-		Triangle triangle5 = new Triangle(p10,p11,p9);
-		Triangle triangle6 = new Triangle(p11,p12,p9);
-		Triangle triangle7 = new Triangle(p11,p14,p12);
-		Triangle triangle8 = new Triangle(p14,p13,p12);
-		
-		ArrayList<Vector3f> points = new ArrayList<Vector3f>();
-		points.add(p1);
-		points.add(p2);
-		points.add(p3);
-		points.add(p4);
-		
-		//nodes
-		Hole hole = new Hole(points);
-		ArrayList<Node> nodesList = new ArrayList<Node>();
-		Node node1 = new Node(triangle1,"1");
-		Node node2 = new Node(triangle2,"2");
-		Node node3 = new Node(triangle3,"3");
-		Node node4 = new Node(triangle4,"4");
-		Node node5 = new Node(triangle5,"5");
-		Node node6 = new Node(triangle6,"6");
-		Node node7 = new Node(triangle7,"7");
-		Node node8 = new Node(triangle8,"8");
-		
-		nodesList.add(node1);
-		nodesList.add(node2);
-		nodesList.add(node3);
-		nodesList.add(node4);
-		nodesList.add(node5);
-		nodesList.add(node6);
-		nodesList.add(node7);
-		nodesList.add(node8);
-		
-		long time1 = System.currentTimeMillis();
-		NodeNetwork net = new NodeNetwork(hole.getNodesNetwork(nodesList));
-		long time2 = System.currentTimeMillis();
-		
-		System.out.println("network's size: "+net.getNodesList().size());
-		System.out.println("Run time: "+(time2-time1));
-		System.out.println();
-		for(Node node:net.getNodesList()){
-			System.out.print("\nNode "+node+" is connected to: ");
-			if(net.getAdjacentNodes(node).size()==0){
-				System.out.print("no Nodes");
-			}else{
-				for(Node connectedNode:net.getAdjacentNodes(node)){
-					System.out.println("node "+connectedNode);
-				}
-				System.out.println();
-			}
-		}
+//		long time1 = System.currentTimeMillis();
+//		NodeNetwork net = new NodeNetwork(hole.getNodesNetwork(nodesList));
+//		long time2 = System.currentTimeMillis();
+//		
+//		System.out.println("network's size: "+net.getNodesList().size());
+//		System.out.println("Run time: "+(time2-time1));
+//		System.out.println();
+//		for(Node node:net.getNodesList()){
+//			System.out.print("\nNode "+node+" is connected to: ");
+//			if(net.getAdjacentNodes(node).size()==0){
+//				System.out.print("no Nodes");
+//			}else{
+//				for(Node connectedNode:net.getAdjacentNodes(node)){
+//					System.out.println("node "+connectedNode);
+//				}
+//				System.out.println();
+//			}
+//		}
 	}
 	public static  float getNumber(){
 		float number = (float) (Math.random()*20);
